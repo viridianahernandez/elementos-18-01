fd1256eb4f5a3d8b80d746303bfd5bdab3d51354
Acer1 <Acer1@10.20.6.94>
2017-10-10 21:10:07 -0500

    mi primer commit

---
+++ src/main/java/org/unitec/elementos1801/Elementos1801Application.java
@@ -0,0 +1,12 @@
+package org.unitec.elementos1801;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elementos1801Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Elementos1801Application.class, args);
+	}
+}
---
+++ src/test/java/org/unitec/elementos1801/Elementos1801ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.unitec.elementos1801;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Elementos1801ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
